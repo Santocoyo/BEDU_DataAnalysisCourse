acf326d7174054ab719e1a0aa485b9e7a08eb35f
Alejandro Santoscoy Rivero <45904717+Santocoyo@users.noreply.github.com>
2020-10-28 21:15:52 -0600

    Update reto2.sql

--- sesion2/reto2.sql
+++ sesion2/reto2.sql
@@ -4,4 +4,4 @@ DESCRIBE puesto;
 SELECT avg(salario) AS promedio FROM puesto;
 SELECT count(*) AS "numero de pastas" FROM articulo WHERE nombre LIKE "%pasta%";
 SELECT min(salario) AS "salaio minimo", max(salario) AS "salario maximo" FROM puesto;
-SELECT sum(salario) AS sume_mejores_salarios FROM puesto WHERE id_puesto>995;
+SELECT sum(salario) AS sume_mejores_salarios FROM puesto WHERE id_puesto>((SELECT max(id_puesto) FROM puesto) - 5);
